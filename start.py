124019dfefb79cba950c54182e17f82b08b76ce9
Roman Yatsenko <roman.yatsenko@hneu.net>
2021-01-09 17:03:34 +0200

    Initial commit

---
+++ start.py
@@ -0,0 +1,21 @@
+import sys
+
+import pygame
+
+def run_game():
+    # Инициализирует игру и создает объект экрана
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Пришельцы 2021")
+
+    # Запуск основного цикла игры
+    while True:
+        # Отслеживание событий клавиатуры и мыши
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Отображение последнего прорисованного экрана
+        pygame.display.flip()
+
+run_game()
