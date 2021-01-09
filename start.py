e0904192ae9c3cd894685f206fc440feae9bf171
Roman Yatsenko <roman.yatsenko@hneu.net>
2021-01-09 17:09:25 +0200

    Add screen background color

--- start.py
+++ start.py
@@ -7,6 +7,8 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((800, 600))
     pygame.display.set_caption("Пришельцы 2021")
+    # Назначение цвета фона
+    bg_color = (230, 230, 230)
 
     # Запуск основного цикла игры
     while True:
@@ -15,6 +17,9 @@ def run_game():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+        # При каждом проходе цикла перерисовывается экран
+        screen.fill(bg_color)
+        
         # Отображение последнего прорисованного экрана
         pygame.display.flip()
 
